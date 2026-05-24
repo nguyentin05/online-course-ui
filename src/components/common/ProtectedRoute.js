e289c4e9b56bb4e912d52dfaca81baf6163e9bd8
Nguyen Trong Tin <tin.nguyen.cs05@gmail.com>
2026-05-24 20:07:41 +0700

    feat: add login pipeline and manage user session

--- src/components/common/ProtectedRoute.js
+++ src/components/common/ProtectedRoute.js
@@ -3,20 +3,16 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { UserContext } from '../../configs/MyContexts';
 
 export default function ProtectedRoute({ allowedRoles = [] }) {
-  // Lấy thông tin user hiện tại từ Global State
-  const user = useContext(UserContext);
+  const [user] = useContext(UserContext);
 
-  // 1. Nếu chưa đăng nhập -> Đẩy về trang Login
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
-  // 2. Nếu route có yêu cầu Role cụ thể, mà user không có role đó -> Đẩy về trang chủ (hoặc báo lỗi 403)
   if (allowedRoles.length > 0 && !allowedRoles.includes(user.role)) {
     // Có thể thay bằng một trang Component <AccessDenied />
     return <Navigate to="/" replace />; 
   }
 
-  // 3. Hợp lệ -> Cho phép đi tiếp vào trang bên trong
   return <Outlet />;
 }
--- src/configs/MyContexts.js
+++ src/configs/MyContexts.js
@@ -3,5 +3,3 @@ import { createContext } from 'react';
 export const UserContext = createContext();
 
 export const EnrollContext = createContext();
-
-export const EnrollDispatchContext = createContext();
